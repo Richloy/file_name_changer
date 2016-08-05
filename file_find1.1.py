4316c8a28f54f6c1d694ca05711974d9ff955b55
richloy <richloy@gmail.com>
2016-08-05 21:41:07 +0100

    Added season button

--- file_find1.1.py
+++ file_find1.1.py
@@ -2,20 +2,23 @@ import sys, os
 from PyQt4 import QtGui, QtCore
 
 class Window(QtGui.QMainWindow):
+    
     def __init__(self):
         super(Window, self).__init__()
+        
         self.name = ""
+        self.season = ""
         self.setGeometry(400, 250, 600, 300)
         self.setWindowTitle("Game Tutorial")
-        #self.setWindowIcon(QtGui.QIcon('ball.png'))
 
         #Select Folder for files   
         self.file_list = []
-        dir_ = QtGui.QFileDialog.getExistingDirectory(None, 'Select a folder:', 'C:\\', QtGui.QFileDialog.ShowDirsOnly)
+        self.dir_ = QtGui.QFileDialog.getExistingDirectory(None, 'Select a folder:', 'C:\\', QtGui.QFileDialog.ShowDirsOnly)
         
-        for file in os.listdir(dir_):
-            if file.endswith('.py') or file.endswith('.mp4'):
+        for file in os.listdir(self.dir_):
+            if file.endswith('.txt') or file.endswith('.mp4'):
                 self.file_list.append(file)
+                
         #Select file with new names
         f = open('rename.txt', 'r')
         
@@ -25,26 +28,35 @@ class Window(QtGui.QMainWindow):
             line_tokens = line.split('\t')
             rename_list.append(line_tokens[1])
         f.close()
-        print(rename_list)
+        
         for i in range(len(rename_list)):
             print(rename_list[i])
+
+        #Create some functionality to GUI
         self.label_series = QtGui.QLabel("Not Selected", self)
         self.label_series.move(300, 10)
-        self.btn = QtGui.QPushButton("Select Text", self)
+        self.label_season = QtGui.QLabel("Not Selected", self)
+        self.label_season.move(300, 45)
+        self.btn = QtGui.QPushButton("Select Series", self)
         self.btn.move(400, 10)
-        self.btn.clicked.connect(self.gettext)
-
-        
+        self.btn.clicked.connect(self.get_text)
+        self.btn = QtGui.QPushButton("Select Season", self)
+        self.btn.move(400, 45)
+        self.btn.clicked.connect(self.get_season)
+        self.btn_convert = QtGui.QPushButton("Select Text", self)
+        self.btn_convert.move(400, 100)
+        self.btn_convert.clicked.connect(self.convert_files)
         
         self.home()
         
-        
-    def print_name(self):
+    def convert_files(self):
 
-        print (self.name)
+        print(self.name)
+        print(self.season)
         
     def home(self):
-        self.print_name()
+        
+        #self.print_name()
         x = 25
         y = 10
         for i in range(len(self.file_list)):
@@ -54,15 +66,27 @@ class Window(QtGui.QMainWindow):
             print(self.file_list[i])
         
         self.show()
-        
-        
 
-    def gettext(self):
-        text, ok = QtGui.QInputDialog.getText(self, 'Text Input Dialog', 'Enter your name:')
+    def get_text(self):
+        
+        text, ok = QtGui.QInputDialog.getText(self, 'Text Input Dialog', 'Enter The Series:')
 	
         if ok:
             self.label_series.setText(str(text))
-            self.name = str(text)            
+            self.name = str(text)
+
+    def get_season(self):
+
+        text, ok = QtGui.QInputDialog.getText(self, 'Text Input Dialog', 'Enter The Season:')
+	
+        if ok:
+            if len(text) < 2:
+                text = "S0" + str(text)
+                self.label_season.setText(text)
+            else:
+                text = "S" + str(text)
+                self.label_season.setText(text)
+            self.season = text
         
     def close_interface(self):
         choice = QtGui.QMessageBox.question(self, 'Exit', "Are you sure?",
