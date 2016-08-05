d82bd22b17b4d0e5dcd24e5b8d3cc78c1e2237a8
richloy <richloy@gmail.com>
2016-08-05 18:57:10 +0100

    new additions

--- file_find1.1.py
+++ file_find1.1.py
@@ -1,11 +1,10 @@
-import sys, os, platform, subprocess
+import sys, os
 from PyQt4 import QtGui, QtCore
-from pathlib import Path
-
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
+        self.name = ""
         self.setGeometry(400, 250, 600, 300)
         self.setWindowTitle("Game Tutorial")
         #self.setWindowIcon(QtGui.QIcon('ball.png'))
@@ -29,19 +28,41 @@ class Window(QtGui.QMainWindow):
         print(rename_list)
         for i in range(len(rename_list)):
             print(rename_list[i])
-        self.home()
+        self.label_series = QtGui.QLabel("Not Selected", self)
+        self.label_series.move(300, 10)
+        self.btn = QtGui.QPushButton("Select Text", self)
+        self.btn.move(400, 10)
+        self.btn.clicked.connect(self.gettext)
 
+        
+        
+        self.home()
+        
+        
+    def print_name(self):
+
+        print (self.name)
+        
     def home(self):
+        self.print_name()
         x = 25
-        y = 50
+        y = 10
         for i in range(len(self.file_list)):
             label = QtGui.QLabel(self.file_list[i], self)
             label.move(x, y)
             y += 15
             print(self.file_list[i])
+        
         self.show()
+        
+        
 
-            
+    def gettext(self):
+        text, ok = QtGui.QInputDialog.getText(self, 'Text Input Dialog', 'Enter your name:')
+	
+        if ok:
+            self.label_series.setText(str(text))
+            self.name = str(text)            
         
     def close_interface(self):
         choice = QtGui.QMessageBox.question(self, 'Exit', "Are you sure?",
